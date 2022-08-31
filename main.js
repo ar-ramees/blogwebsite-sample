377c1878e96265004981530666cacde01a5a9930
IAmBatMan-Ramees <rameesahmed6@gmail.com>
2022-08-31 17:01:18 +0530

    Home page with article posts

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ let collapse = document.querySelectorAll("#header .collapse");
 toggle.addEventListener('click',function(){
     collapse.forEach(col=>col.classList.toggle("collapse-toggle"));
 })
+
+//with masonry
+
+new Masonry("#posts .grid",{
+    itemSelector :'.grid-item',
+    gutter : 20
+});
