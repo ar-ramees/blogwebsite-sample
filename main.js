884fd6dfbaa8c78971397bb119998c225ca0678a
IAmBatMan-Ramees <rameesahmed6@gmail.com>
2022-09-01 02:07:41 +0530

    slider

--- main.js
+++ main.js
@@ -6,8 +6,36 @@ toggle.addEventListener('click',function(){
 })
 
 //with masonry
-
 new Masonry("#posts .grid",{
     itemSelector :'.grid-item',
     gutter : 20
 });
+
+//Swiper library initialization
+new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 5,
+    autoplay: {
+        delay: 3000
+    },
+    //responsive breakpoints
+    breakpoints: {
+        '@0': {
+            slidesPerView: 2
+        },
+        // 888px
+        880: {
+            slidesPerView: 3
+        },
+        // 1110px
+        1072: {
+            slidesPerView: 4
+        },
+        // 1330px
+        1272: {
+            slidesPerView: 5
+        }
+    }
+})
+
